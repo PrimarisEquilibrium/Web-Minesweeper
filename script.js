d1571b407f0a1859be1f991f1f7a97d53c45687e
PrimarisEquilibrium <incorrectclock1@gmail.com>
2025-03-23 11:18:45 -0400

    feat: create clickable minesweeper cell

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello, World!")
+/**
+ * Represents a minesweeper board (width x height)
+ */
+class Board {
+    constructor(width, height) {
+        this.width = width
+        this.height = height
+    }
+
+    draw() {
+        console.log("Drawing board...")
+    }
+}
+
+const boardDiv = document.createElement("div")
+boardDiv.classList.add("board")
+const cellDiv = document.createElement("div")
+cellDiv.classList.add("cell")
+cellDiv.addEventListener("click", (event) => {
+    cellDiv.classList.toggle("clicked")
+})
+
+boardDiv.appendChild(cellDiv)
+document.body.appendChild(boardDiv)
+
+let board = new Board(10, 10)
+board.draw()
